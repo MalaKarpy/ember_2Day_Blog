e05381c4ef2d90f98e6cf979aeecbf985687c8b2
colleen minor & mala karpy <student@epicodus.com>
2015-09-30 09:37:18 -0700

    ADD bLOG INITIAL PROJ

---
+++ server/mocks/posts.js
@@ -0,0 +1,53 @@
+module.exports = function(app) {
+  var express = require('express');
+  var postsRouter = express.Router();
+
+  postsRouter.get('/', function(req, res) {
+    res.send({
+      'posts': [
+        {
+          id: 1,
+          title: 'Bananas',
+          author: 1
+        },
+        {
+          id: 2,
+          title: 'Apples',
+          author: 1
+
+        }
+      ],
+      "authors": [{
+        id:1,
+        name: "Teresa",
+        posts: [1,2]
+      }]
+    });
+  });
+
+  postsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  postsRouter.get('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.put('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/posts', postsRouter);
+};
